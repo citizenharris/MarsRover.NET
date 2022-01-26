02c5d605ea0e460d0c15e98456e56c4ac5f2f531
Aman Panesar <amanpanesar@Amans-MacBook-Pro-2.local>
2022-01-26 11:07:06 +0000

    loop over commands

--- MarsRover/MarsRover.cs
+++ MarsRover/MarsRover.cs
@@ -2,30 +2,22 @@
 
 public class MarsRover
 {
-    private string _xCoordinate;
-    private string _separator;
-    private string _yCoordinate;
+    private int _xCoordinate;
+    private int _yCoordinate;
     private string _direction;
 
     public string Execute(string commands)
     {
-        _separator = ":";
         _direction = "N";
         
-        if (commands == "MMM")
+        foreach (var command in commands)
         {
-            _xCoordinate = "0";
-            _yCoordinate = "3";
-            return _xCoordinate+_separator+_yCoordinate+_separator+_direction;
+            if (command == 'M')
+            {
+                _yCoordinate += 1;
+            }
         }
 
-        if (commands == "MM")
-        {
-            _xCoordinate = "0";
-            _yCoordinate = "2";
-            return _xCoordinate+_separator+_yCoordinate+_separator+_direction;
-        }
-
-        return "0:1:N";
+        return $"{_xCoordinate}:{_yCoordinate}:{_direction}";
     }
 }
